2cd94c476592094bdf8c80d3a99db5b7f86c1d35
mivanovsjelgava <freeutka@inbox.lv>
2026-06-22 13:04:29 +0300

    Hello world!

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,11 +1,22 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { appName, gitConfig } from './shared';
+import Image from 'next/image';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      // JSX supported
-      title: appName,
+      title: (
+        <div className="flex items-center gap-2.5">
+          <Image 
+            src="/logo.svg" 
+            alt={`${appName} Logo`} 
+            width={24} 
+            height={24} 
+            priority
+          />
+          <span className="font-bold text-lg">{appName}</span>
+        </div>
+      ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
